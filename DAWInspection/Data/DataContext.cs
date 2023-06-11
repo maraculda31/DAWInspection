e29dd3f39fa987c605021ac86ba9a7034b4a0d94
maraculda31 <culdamara@gmail.com>
2023-06-11 21:37:06 +0300

    third commit

--- DAWInspection/Data/DataContext.cs
+++ DAWInspection/Data/DataContext.cs
@@ -1,6 +1,16 @@
-﻿namespace DAWInspection.Data
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAWInspection.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
     }
 }
--- DAWInspection/Inspection.cs
+++ DAWInspection/Inspection.cs
@@ -1,6 +1,21 @@
-﻿namespace DAWInspection
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAWInspection
 {
     public class Inspection
     {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public string Comments { get; set; } = string.Empty;
+
+        public int InspectionTypeId { get; set; }
+
+        public InspectionType? InspectionType { get; set; }
+
+
     }
 }
